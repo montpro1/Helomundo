142ce656b57b93d87bee72b4a8d109521bd05130
Víctor Enrique Montes Tovar <vmontestovar@nautilus19.ad.danielcastelao.org>
2019-10-16 13:32:36 +0200

    SIN COMMITS

---
+++ helomundo/Helomundo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helomundo;
+
+/**
+ *
+ * @author vmontestovar
+ */
+public class Helomundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("HOLLLLLA MUNDO");
+        // TODO code application logic here
+    }
+    
+}
